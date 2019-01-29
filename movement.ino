4d08523f7205cd720f5cabfcdac97731663f0141
Alex Newgent <newgent@terpmail.umd.edu>
2019-01-29 13:44:53 -0500

    Update and rename ping.ino to movement.ino

---
+++ movement.ino
@@ -0,0 +1,108 @@
+// defines pins numbers
+const int LEFT_FWD      = 10;
+const int LEFT_BWD      = 11;
+const int LEFT_PWM      = 5;
+const int RIGHT_FWD     = 12;
+const int RIGHT_BWD     = 13;
+const int RIGHT_PWM     = 6;
+const int SENSOR_LEFT   = 7;
+const int SENSOR_RIGHT  = 8;
+
+const int LEFT_SPEED    = 130;
+const int RIGHT_SPEED   = 130;
+
+const int OBS_DIST      = 10;
+void setup() {
+  // initialize serial communication:
+  pinMode(LEFT_FWD, OUTPUT);
+  pinMode(LEFT_BWD, OUTPUT);
+  pinMode(LEFT_PWM, OUTPUT);
+  pinMode(RIGHT_FWD, OUTPUT);
+  pinMode(RIGHT_BWD, OUTPUT);
+  pinMode(RIGHT_PWM, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // establish variables for duration of the ping, and the distance result
+  // in inches and centimeters:
+  long duration1, duration2, cm1, cm2;
+
+
+  /***************************************************************************
+   * SENSOR CODE
+   ***************************************************************************/
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+
+  cm1 = ping(SENSOR_LEFT);
+  cm2 = ping(SENSOR_RIGHT);
+
+  Serial.print(cm1);
+  Serial.print(" cm, ");
+  Serial.print(cm2);
+  Serial.print(" cm");
+  
+  Serial.println();
+
+  delay(100);
+
+  
+  /***************************************************************************
+   * MOTOR CODE
+   ***************************************************************************/
+   move_fwd(130,130);
+   delay(400);
+   move_fwd(-130,-130);
+   delay(400);
+   move_fwd(0,0);
+}
+
+int ping(int p){
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(p, OUTPUT);
+  digitalWrite(p, LOW);
+  delayMicroseconds(2);
+  digitalWrite(p, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(p, LOW);
+
+  pinMode(p, INPUT);
+
+  return pulseIn(p, HIGH)*.034/2;
+}
+
+void move_fwd (int l_pwm, int r_pwm) {
+  //Write the speed to the motors
+  analogWrite(LEFT_PWM, abs(l_pwm));
+  analogWrite(RIGHT_PWM,abs(r_pwm));
+
+  // Determine the direction the motors should go
+  digitalWrite(LEFT_FWD,  (l_pwm >= 0));
+  digitalWrite(RIGHT_FWD, (r_pwm >= 0));
+  digitalWrite(LEFT_BWD,  (l_pwm < 0));
+  digitalWrite(RIGHT_BWD, (r_pwm < 0));
+}
+
+void check_obstacles () {
+  // The same pin is used to read the signal from the PING))): a HIGH pulse
+  // whose duration is the time (in microseconds) from the sending of the ping
+  // to the reception of its echo off of an object.
+  long duration1, duration2, l_dist, r_dist;
+
+  l_dist = ping(SENSOR_LEFT);
+  r_dist = ping(SENSOR_RIGHT);
+
+  Serial.print(l_dist);
+  Serial.print(" cm, ");
+  Serial.print(r_dist);
+  Serial.print(" cm");
+  Serial.println();
+
+  move_fwd(LEFT_SPEED*(1-2*(l_dist <= OBS_DIST)),
+      RIGHT_SPEED*(1-2*(r_dist <= OBS_DIST)));
+  delay(100);
+
+}
