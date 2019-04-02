8b6621349e812ce06e4b8d4e0a36c996a02a9721
chrisossi1 <chrisossi1@gmail.com>
2019-04-02 11:54:42 -0400

    Add files via upload

---
+++ alexa_skill.py
@@ -0,0 +1,47 @@
+import logging
+
+from flask import Flask, render_template
+
+from flask_ask import Ask, statement, question, session
+
+
+app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+hinumber=0
+
+@ask.launch
+
+def startup():
+
+    welcome_msg = render_template('welcome')
+
+    return question(welcome_msg)
+
+
+@ask.intent("hi")
+
+def hi():
+	hinumber+=1
+    msg = render_template('hi', number=hinumber)
+
+    return statement(msg)
+
+
+@ask.intent("addthree", convert={'first': int, 'second': int, 'third': int})
+
+def answer(first, second, third):
+
+    sum = first+second+third
+
+    msg = render_template('addthree',number = sum)
+
+    return statement(msg)
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
