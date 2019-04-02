3bcc04f31295651d4f5af1e4b4f7776f14851731
chrisossi1 <chrisossi1@gmail.com>
2019-04-02 12:03:54 -0400

    Update alexa_skill.py

--- alexa_skill.py
+++ alexa_skill.py
@@ -13,35 +13,26 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 hinumber=0
 
+
 @ask.launch
-
 def startup():
-
     welcome_msg = render_template('welcome')
-
     return question(welcome_msg)
 
 
 @ask.intent("hi")
-
 def hi():
-	hinumber+=1
+    hinumber += 1
     msg = render_template('hi', number=hinumber)
-
     return statement(msg)
 
 
 @ask.intent("addthree", convert={'first': int, 'second': int, 'third': int})
-
 def answer(first, second, third):
-
-    sum = first+second+third
-
-    msg = render_template('addthree',number = sum)
-
+    result = first+second+third
+    msg = render_template('addthree',number = result)
     return statement(msg)
 
 
 if __name__ == '__main__':
-
     app.run(debug=True)
